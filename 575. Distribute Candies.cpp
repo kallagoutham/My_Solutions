b4a5eef533975b18512635eed6e2874c792fb165
kallagoutham <82465870+kallagoutham@users.noreply.github.com>
2023-04-04 23:08:34 +0530

    Update 575. Distribute Candies.cpp

--- 575. Distribute Candies.cpp
+++ 575. Distribute Candies.cpp
@@ -8,3 +8,18 @@ public:
         return min(m.size(),candyType.size()/2);
     }
 };
+
+--------------------------------------------------------------------------------
+//Using the bitset of the c++ bitset header file
+    
+class Solution {
+ public:
+  int distributeCandies(vector<int>& candies) {
+    bitset<200001> bitset;
+
+    for (const int candy : candies)
+      bitset.set(candy + 100000);
+
+    return min(candies.size() / 2, bitset.count());
+  }
+};
