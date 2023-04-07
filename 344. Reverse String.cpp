25a43afdac1db766b92f1c1c2a7fce7723233267
kallagoutham <82465870+kallagoutham@users.noreply.github.com>
2023-04-07 13:28:23 +0530

    Create 344. Reverse String.cpp

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
+/*---------------------------------------------------------------*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            char temp=s[i];
+            s[i]=s[n-1-i];
+            s[n-1-i]=temp;
+        }
+    }
+};
