77beabebfcbe0be269568c34110bf84785f0caad
kallagoutham <82465870+kallagoutham@users.noreply.github.com>
2023-09-04 00:57:12 +0530

    Update 905. Sort Array By Parity.cpp

--- 905. Sort Array By Parity.cpp
+++ 905. Sort Array By Parity.cpp
@@ -16,3 +16,21 @@ public:
         return nums;
     }
 };
+------------------------------------------------------------------------------------------------
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        unordered_map<int,vector<int>> map;   
+        for(auto i:nums){
+            map[i&1].push_back(i);
+        }
+        if(map.find(1)==map.end()){
+            return map[0];
+        }
+        if(map.find(0)==map.end()){
+            return map[1];
+        }
+        map[0].insert(map[0].end(),map[1].begin(),map[1].end());    //insert at the map[0].end() from map[1].begin() to map[1].end()
+        return map[0];
+    }
+};
