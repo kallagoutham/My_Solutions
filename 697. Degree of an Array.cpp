d84166dd66889bb29be85dfb0fd3cd32a87c2794
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2024-02-10 20:23:06 +0530

    Create 697. Degree of an Array.cpp

---
+++ 697. Degree of an Array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        unordered_map<int,int> first,count;
+        int res=0,degree=0;
+        for(int i=0;i<nums.size();i++){
+            if(first.count(nums[i])==0){first[nums[i]]=i;}
+            if(++count[nums[i]]>degree){
+                degree=count[nums[i]];
+                res=i-first[nums[i]]+1;
+            }else if(count[nums[i]]==degree){
+                res=min(res,i-first[nums[i]]+1);
+            }
+        }
+        return res;
+    }
+};
