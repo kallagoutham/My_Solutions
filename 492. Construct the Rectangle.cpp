4e3c32052ffcecc974af2ff513ec6d73369b4e72
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2024-07-18 16:55:20 +0530

    Update 492. Construct the Rectangle.cpp

--- 492. Construct the Rectangle.cpp
+++ 492. Construct the Rectangle.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        vector<int> res(2,0);
-        int d=INT_MAX;
-        for(int i=area;i>=1;i--){
-            if(area%i==0){
-                if(d>(abs(i-area/i))){
-                    res[0]=max(i,area/i);
-                    res[1]=min(i,area/i);
-                    d=abs(i-area/i);
-                }
-            }
+        int width = static_cast<int>(sqrt(area));
+        while (area % width != 0) {
+            width--;
         }
-        return res;
+        int length = area / width;
+        return {length, width};
     }
 };
