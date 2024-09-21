a76dc3a2ca36b3d9c8ad9b182350fc50fdbec8b5
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2024-09-21 15:23:56 -0400

    Create 386. Lexicographical Numbers.cpp

---
+++ 386. Lexicographical Numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<string> res;
+        for(int i=1;i<=n;++i){
+            res.push_back(to_string(i));
+        }
+        sort(res.begin(),res.end());
+        vector<int> ans;
+        for(string i:res){
+            ans.push_back(stoi(i));
+        }
+        return ans;
+    }
+};
