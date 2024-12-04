9e4ac36ae36bcfb16533bb0acda9e0863ccaacdf
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2024-12-03 21:23:04 -0500

    Create 2825. Make String a Subsequence Using Cyclic Increments.cpp

---
+++ 2825. Make String a Subsequence Using Cyclic Increments.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int i = 0, j = 0; 
+                while (i < str1.size() && j < str2.size()) {
+            if (str1[i] == str2[j]) {
+                j++;
+            } else {
+                char expected = (str1[i] == 'z') ? 'a' : str1[i] + 1;
+                if (expected == str2[j]) {
+                    j++;
+                }
+            }
+            i++;
+        }
+        return j == str2.size();
+    }
+};
