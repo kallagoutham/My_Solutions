f3da9c1ef1149672c02e032042c2bc77a7ca9785
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2025-01-22 19:37:20 -0500

    Create 1267. Count Servers that Communicate.py

---
+++ Leetcode/1267. Count Servers that Communicate.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m,n=len(grid),len(grid[0])
+        row=[0]*m
+        col=[0]*n
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    row[i]+=1
+                    col[j]+=1
+        res=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    if row[i]>1 or col[j]>1:
+                        res+=1
+        return res
+        
