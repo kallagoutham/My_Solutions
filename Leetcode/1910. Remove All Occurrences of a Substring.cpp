5dd0a517c1fe70e0797138453211042630d9eb5e
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2025-02-10 23:26:04 -0500

    Create 1910. Remove All Occurrences of a Substring.cpp

---
+++ Leetcode/1910. Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.find(part) != string::npos) {
+            int idx = s.find(part);
+            s = s.substr(0, idx) + s.substr(idx + part.length());
+        }
+        return s;
+    }
+};
