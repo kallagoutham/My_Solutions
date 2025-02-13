1056982c52c8b823217d1f798750a9f47d410fca
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2025-02-12 20:25:36 -0500

    Create 3066. Minimum Operations to Exceed Threshold Value II.cpp

---
+++ Leetcode/3066. Minimum Operations to Exceed Threshold Value II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& n, int k) {
+        int res = 0;
+        priority_queue<long, vector<long>, greater<long>> pq;
+        for(int i : n) {
+            pq.push(static_cast<long>(i));
+        }
+        while(pq.top() < k) {
+            res++;
+            long ft = pq.top();
+            pq.pop();
+            long st = pq.top();
+            pq.pop();
+            long temp = static_cast<long>(min(ft, st)) * 2 + max(ft, st);
+            pq.push(temp);
+        }
+        return res;
+    }
+};
