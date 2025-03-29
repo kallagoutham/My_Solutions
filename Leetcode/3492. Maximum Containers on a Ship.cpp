f18972640876f6f31e23b955b9e313a7b47a046b
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2025-03-28 22:19:28 -0400

    Create 3492. Maximum Containers on a Ship.cpp

---
+++ Leetcode/3492. Maximum Containers on a Ship.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxContainers(int n, int w, int maxWeight) {
+        if((n*n)*w <= maxWeight){
+            return n*n;
+        }
+        return maxWeight/w;
+    }
+};
