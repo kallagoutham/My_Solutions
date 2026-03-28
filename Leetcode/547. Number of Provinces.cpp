b3dabca681b423432b826a2ecd164dbc01f6caf3
Goutham Kalla <82465870+kallagoutham@users.noreply.github.com>
2026-03-28 02:09:13 -0400

    Implement Union-Find for Number of Provinces
    Added Union-Find implementation to solve the problem of counting provinces.

--- Leetcode/547. Number of Provinces.cpp
+++ Leetcode/547. Number of Provinces.cpp
@@ -24,3 +24,54 @@ public:
         return res;
     }
 };
+
+
+// Using Union Find Solution 
+
+class UnionFind{
+    private:
+        vector<int> parent,rank;
+    public:
+        UnionFind(int size){
+            rank.resize(size,0);
+            parent.resize(size);
+            for(int i=0;i<size;i++){
+                parent[i]=i;
+            }
+        }
+        int findParent(int node){
+            if(node==parent[node]){return node;}
+            return parent[node] = findParent(parent[node]);
+        }
+        void union_set(int x,int y){
+            int ulp_x = findParent(x);
+            int ulp_y = findParent(y);
+            if(ulp_x == ulp_y){return;}
+            if(rank[ulp_x] < rank[ulp_y]){
+                parent[ulp_x] = ulp_y;
+            }else if(rank[ulp_y] < rank[ulp_x]){
+                parent[ulp_y] = ulp_x;
+            }else{
+                parent[ulp_y] = ulp_x;
+                rank[ulp_x]++;
+            }
+        }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        UnionFind dsu(n);
+        int numberOfComponents = n;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j] && dsu.findParent(i) != dsu.findParent(j)){
+                    numberOfComponents--;
+                    dsu.union_set(i,j);
+                }
+            }
+        }
+        return numberOfComponents;
+    }
+};
